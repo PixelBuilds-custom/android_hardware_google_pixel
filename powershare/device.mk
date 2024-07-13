c03d21dd80cc1477a78b743f63700c9dcefb4568
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2024-07-13 14:06:49 +0300

    pixel: powershare: include lineage FCM
    Change-Id: Ia5a598dec6b872a684a8a9ac32a010902fd20457

--- powershare/device.mk
+++ powershare/device.mk
@@ -3,3 +3,4 @@ PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.pixel
 
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powershare
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/pb/config/device_framework_matrix.xml
